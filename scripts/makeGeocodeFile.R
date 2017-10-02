bdcaabb5956914b29f6a8c247dca598f008d63e1
Leon Stirk-Wang <leon.stirkwang@gmail.com>
2017-10-02 07:34:03 +0000

    updates
    Former-commit-id: 223fb069e465a59a5adbf5e13b9c8e1a05b479e5

--- scripts/makeGeocodeFile.R
+++ scripts/makeGeocodeFile.R
@@ -1,7 +1,7 @@
 #!/usr/bin/env Rscript
 
+load("/home/ubuntu/rstats/datasets/dccRates.Rda")
 
 # Create a motherfucking dataframe for geocoding and save that motherfucker to a csv file for export.
 geo <- df[,c("ID", "PropertyDetails.PropertyAddress", "PropertyDetails.PostalAddressForThisAssessment")]
 write.csv(x = geo, file = "dccGeoSrc.csv")
-
