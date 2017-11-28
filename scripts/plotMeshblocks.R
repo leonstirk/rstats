93739fb39ebf28abc96d678f0183eae7037ae316
Leon Stirk-Wang <leon.stirkwang@gmail.com>
2017-11-29 01:30:02 +1300

    auto-backup
    Former-commit-id: 6d90b112f5b69352c8e68c940d0fa09a9b719f96
    Former-commit-id: 5717d77ac5959d12da1ed0493d9562b1a4fb4c15

---
+++ scripts/plotMeshblocks.R
@@ -0,0 +1,34 @@
+#!/usr/bin/env Rscript
+
+library(maptools)
+
+library(RColorBrewer)
+colors <- brewer.pal(9, "BuGn")
+
+mapImage <- get_map(location = c(lon=174.7633, lat=-36.8485), zoom = 9)
+
+proj4string <- "+proj=nzmg +lat_0=-41.0 +lon_0=173.0 +x_0=2510000.0 +y_0=6023150.0 +ellps=intl +units=m"
+
+area <- readShapePoly("/home/ubuntu/rstats/shapefiles/MB_RC_Auckland_RC.shp")
+
+area.points <- fortify(area)
+
+pj <- project(area.points[,c("long", "lat")], proj4string, inverse = TRUE)
+
+latlon <- data.frame(lat=pj$y, long=pj$x)
+
+area.points <- rename(area.points, c("long" = "NZMGlong", "lat" = "NZMGlat"))
+
+area.points$lat <- latlon$lat
+area.points$long <- latlon$lon
+
+ggmap(mapImage) +
+  geom_polygon(aes(x = long,
+      y = lat,
+      group = group),
+    data = area.points,
+    color = colors[9],
+    fill = colors[6],
+    alpha = 0.5) +
+labs(x = "Longitude",
+  y = "Latitude")
