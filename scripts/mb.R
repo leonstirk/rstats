5e6f610b1bc359c216a8992a2a8fcf63e97c601b
Leon Stirk-Wang <leon.stirkwang@gmail.com>
2018-02-01 01:30:01 +1300

    auto-backup
    Former-commit-id: 0d4da5af8bea5b152f2356d15a840964b1f672f4
    Former-commit-id: bfb831770d66eb61dd304375df5dfa89d7340990

--- scripts/mb.R
+++ scripts/mb.R
@@ -5,31 +5,53 @@ library('reshape2')
 
 a<-read.csv("/home/ubuntu/rstats/datasets/2013-mb-dataset-Total-New-Zealand-Dwelling.csv")
 
-mb<-c[1:46629,]
-au<-c[46631:48642,]
-wd<-c[48644:48901,]
-ta<-c[48903:48970,]
-rn<-c[48972:48988,]
-lba<-c[48990:49010,]
+mb<-a[1:46629,]
+# au<-a[46631:48642,]
+# wd<-a[48644:48901,]
+# ta<-a[48903:48970,]
+# rn<-a[48972:48988,]
+# lba<-a[48990:49010,]
+tmp<-colsplit(mb$Area_Code_and_Description," ",c("text","code"))
+mb$mb<-tmp$code
 
-a_long<-gather(a, key, value, names(a[4:ncol(a)]))
+drops <- c("Area_Code_and_Description","Code","Description")
+mb<-mb[ , !(names(mb) %in% drops)]
 
-b<-colsplit(a_long$key, "_", names=c("year_a", "text"))
+mb_cols<-ncol(mb)
 
+key<-read.csv("/home/ubuntu/rstats/datasets/mbKey.csv")
+tmp<-colsplit(key$Meshblock..2013.areas.," ",c("text","code"))
+key$mb<-tmp$code
+drops<- c("Meshblock..2013.areas.")
+key<-key[,!(names(key) %in% drops)]
+
+centroid<-read.csv("/home/ubuntu/rstats/datasets/mb2013Centroid.csv")
+centroid$mb<-centroid$MB2013_V1_00
+drops<-c("MB2013_V1_00")
+centroid<-centroid[,!(names(centroid) %in% drops)]
+
+merge1<-merge(mb,key,by=c("mb"))
+merge2<-merge(merge1,centroid,by=c("mb"))
+
+mb_long<-gather(merge2, key, value, names(merge2[2:mb_cols]))
+
+b<-colsplit(mb_long$key, "_", names=c("year_a", "text"))
 b$year_b<-substring(b$year_a,2,nchar(b$year_a))
 b$year<-as.numeric(b$year_b)
 b$factor<-as.factor(b$text)
 
-c<-cbind(a_long,b)
+c<-cbind(mb_long,b)
 
-c[c==""]<-NA
 c$confidential<-as.numeric(c$value=="..C")
 c$value[c$value=="..C"]<-NA
 c$value<-as.numeric(c$value)
+drops<-c("year_a","year_b","text", "OBJECTID", "key")
+c<-c[,!(names(c) %in% drops)]
 
+names<-c("mbCode2013","auCode2013","auName2013","wdCode2013","wdName2013","taCode2013","taName2013","rcCode2013","rcName2013","landTypeCode2013","landTypeName2013","areaTotalSqKm","areaLandSqKm","NZTM2000Easting","NZTM2000Northing","WGS84Lat","WGS84Lon","value","year","key","confidential")
 
-key<-read.csv("/home/ubuntu/rstats/datasets/mbKey.csv")
-centroid<-read.csv("/home/ubuntu/rstats/datasets/meshblock-2018-centroid-true.csv")
+names(c)<-names
 
+# c[c==""]<-NA
 
-# merge(mb, mbKey, by = c("
+mb_wide<-spread(c,key,value)
