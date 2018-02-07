a1df51983ca709cd6190a7725af3e8f4ae3f8c23
Leon Stirk-Wang <leon.stirkwang@gmail.com>
2018-02-08 01:30:11 +1300

    auto-backup
    Former-commit-id: 462ff00230b791d3402103d56110b9e471a0aa82
    Former-commit-id: fc68c5b89bf5b1e191ba0650184761d82fdb70a8

--- scripts/mb.R
+++ scripts/mb.R
@@ -14,6 +14,8 @@ mb<-a[1:46629,]
 tmp<-colsplit(mb$Area_Code_and_Description," ",c("text","code"))
 mb$mb<-tmp$code
 
+rm(tmp)
+
 drops <- c("Area_Code_and_Description","Code","Description")
 mb<-mb[ , !(names(mb) %in% drops)]
 
@@ -25,33 +27,54 @@ key$mb<-tmp$code
 drops<- c("Meshblock..2013.areas.")
 key<-key[,!(names(key) %in% drops)]
 
+rm(tmp)
+
 centroid<-read.csv("/home/ubuntu/rstats/datasets/mb2013Centroid.csv")
 centroid$mb<-centroid$MB2013_V1_00
 drops<-c("MB2013_V1_00")
 centroid<-centroid[,!(names(centroid) %in% drops)]
 
-merge1<-merge(mb,key,by=c("mb"))
-merge2<-merge(merge1,centroid,by=c("mb"))
+mb<-merge(mb,key,by=c("mb"))
+mb<-merge(mb,centroid,by=c("mb"))
 
-mb_long<-gather(merge2, key, value, names(merge2[2:mb_cols]))
+mb_long<-gather(mb, key, value, names(mb[2:mb_cols]))
 
-b<-colsplit(mb_long$key, "_", names=c("year_a", "text"))
-b$year_b<-substring(b$year_a,2,nchar(b$year_a))
-b$year<-as.numeric(b$year_b)
-b$factor<-as.factor(b$text)
+tmp<-colsplit(mb_long$key, "_", names=c("year_a", "text"))
+tmp$year_b<-substring(tmp$year_a,2,nchar(tmp$year_a))
+tmp$year<-as.numeric(tmp$year_b)
+tmp$factor<-as.factor(tmp$text)
 
-c<-cbind(mb_long,b)
+mb_long<-cbind(mb_long,tmp)
 
-c$confidential<-as.numeric(c$value=="..C")
-c$value[c$value=="..C"]<-NA
-c$value<-as.numeric(c$value)
+rm(tmp)
+
+mb_long$confidential<-as.numeric(mb_long$value=="..C")
+mb_long$value[mb_long$value=="..C"]<-NA
+mb_long$value<-as.numeric(mb_long$value)
 drops<-c("year_a","year_b","text", "OBJECTID", "key")
-c<-c[,!(names(c) %in% drops)]
+mb_long<-mb_long[,!(names(mb_long) %in% drops)]
 
 names<-c("mbCode2013","auCode2013","auName2013","wdCode2013","wdName2013","taCode2013","taName2013","rcCode2013","rcName2013","landTypeCode2013","landTypeName2013","areaTotalSqKm","areaLandSqKm","NZTM2000Easting","NZTM2000Northing","WGS84Lat","WGS84Lon","value","year","key","confidential")
 
-names(c)<-names
+names(mb_long)<-names
+
+mb_long$key_char<-as.character(mb_long$key)
+tmp<-colsplit(mb_long$key_char, "_(?=[A-Z])", names=c("something", "something_else"))
+tmp$something<-as.factor(tmp$something)
+tmp$something_else<-as.factor(tmp$something_else)
+
+mb_long<-cbind(mb_long,tmp)
+
+rm(tmp)
+
+tmp<-mb_long[which(mb_long$something==levels(mb_long$something)[1]),]
+
+drops<-c("key_char","something","key")
+tmp<-tmp[,!(names(tmp) %in% drops)]
+
+tmp_wide<-spread(tmp,something_else,value)
 
 # c[c==""]<-NA
 
-mb_wide<-spread(c,key,value)
+# mb_wide<-spread(d,key,value)
+# mb_wide<-spread(e,something,something_else)
