9e2075a93801d3bcf0ba6cf45cda73a40a2db969
Leon Stirk-Wang <leon.stirkwang@gmail.com>
2018-02-15 01:30:04 +1300

    auto-backup
    Former-commit-id: fd2f576f0da5aa5fc2d954d3c35453f2fabd6ba8
    Former-commit-id: 9325801be78747050b734cec2458f8825a3f3d55

--- scripts/mb.R
+++ scripts/mb.R
@@ -1,9 +1,13 @@
 #!/usr/bin/env Rscript
 
+rm(list = ls())
+
 library('tidyr')
 library('reshape2')
 
-a<-read.csv("/home/ubuntu/rstats/datasets/2013-mb-dataset-Total-New-Zealand-Household.csv")
+# load("/home/ubuntu/rstats/datasets/blocks.R")
+
+a<-read.csv("/home/ubuntu/rstats/datasets/2013-mb-dataset-Total-New-Zealand-Individual-Part-3b.csv")
 
 mb<-a[1:46629,]
 # au<-a[46631:48642,]
@@ -21,23 +25,7 @@ mb<-mb[ , !(names(mb) %in% drops)]
 
 mb_cols<-ncol(mb)
 
-key<-read.csv("/home/ubuntu/rstats/datasets/mbKey.csv")
-tmp<-colsplit(key$Meshblock..2013.areas.," ",c("text","code"))
-key$mb<-tmp$code
-drops<- c("Meshblock..2013.areas.")
-key<-key[,!(names(key) %in% drops)]
-
-rm(tmp)
-
-centroid<-read.csv("/home/ubuntu/rstats/datasets/mb2013Centroid.csv")
-centroid$mb<-centroid$MB2013_V1_00
-drops<-c("MB2013_V1_00")
-centroid<-centroid[,!(names(centroid) %in% drops)]
-
-mb<-merge(mb,key,by=c("mb"))
-mb<-merge(mb,centroid,by=c("mb"))
-
-mb_long<-gather(mb, key, value, names(mb[2:mb_cols]))
+mb_long<-gather(mb, key, value, names(mb[1:mb_cols-1]))
 
 tmp<-colsplit(mb_long$key, "_", names=c("year_a", "text"))
 tmp$year_b<-substring(tmp$year_a,2,nchar(tmp$year_a))
@@ -48,17 +36,19 @@ mb_long<-cbind(mb_long,tmp)
 
 rm(tmp)
 
-mb_long$confidential<-as.numeric(mb_long$value=="..C")
-mb_long$value[mb_long$value=="..C"]<-NA
-mb_long$value<-as.numeric(mb_long$value)
-drops<-c("year_a","year_b","text", "OBJECTID", "key")
+drops<-c("year_a","year_b","text", "key")
 mb_long<-mb_long[,!(names(mb_long) %in% drops)]
 
-names<-c("mbCode2013","auCode2013","auName2013","wdCode2013","wdName2013","taCode2013","taName2013","rcCode2013","rcName2013","landTypeCode2013","landTypeName2013","areaTotalSqKm","areaLandSqKm","NZTM2000Easting","NZTM2000Northing","WGS84Lat","WGS84Lon","value","year","key","confidential")
+names(mb_long)<-c("mb","value","year","key")
 
-names(mb_long)<-names
+mb_wide<-spread(mb_long, key, value)
 
-df<-spread(mb_long,key,value)
+
+
+
+
+
+
 
 # mb_long$key_char<-as.character(mb_long$key)
 # tmp<-colsplit(mb_long$key_char, "_(?=[A-Z])", names=c("something", "something_else"))
