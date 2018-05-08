47c9805c3cb105044ea457ab59f1d76c492d7ce3
Leon Stirk-Wang <leon.stirkwang@gmail.com>
2018-05-09 01:30:02 +1200

    auto-backup
    Former-commit-id: f937cf7cef6836f9912153389b26f117c3afe125
    Former-commit-id: 9654761c9d292fe0a60a44671a4385dc6a21205d

--- scripts/plotMeshblocks.R
+++ scripts/plotMeshblocks.R
@@ -1,6 +1,9 @@
 #!/usr/bin/env Rscript
 
+library(plyr)
+library(ggmap)
 library(maptools)
+library(proj4)
 
 library(RColorBrewer)
 colors <- brewer.pal(9, "BuGn")
@@ -9,7 +12,7 @@ mapImage <- get_map(location = c(lon=174.7633, lat=-36.8485), zoom = 9)
 
 proj4string <- "+proj=nzmg +lat_0=-41.0 +lon_0=173.0 +x_0=2510000.0 +y_0=6023150.0 +ellps=intl +units=m"
 
-area <- readShapePoly("/home/ubuntu/rstats/shapefiles/MB_RC_Auckland_RC.shp")
+area <- readShapePoly("/home/ubuntu/rstats/shapefiles/MB_RC_Auckland_RC")
 
 area.points <- fortify(area)
 
