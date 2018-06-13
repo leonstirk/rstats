ab06246f89028c82dbf98c1b656407442e6f1827
Leon Stirk-Wang <leon.stirkwang@gmail.com>
2018-06-14 01:30:01 +1200

    auto-backup
    Former-commit-id: 81a3195959ccf5a3d5d105573097ebdf7794194a
    Former-commit-id: 8aa5aa55afaa8e9a4e81bfc1c28c1b3827baa343

--- scripts/plotMeshblocks.R
+++ scripts/plotMeshblocks.R
@@ -8,12 +8,19 @@ library(proj4)
 library(RColorBrewer)
 colors <- brewer.pal(9, "BuGn")
 
-mapImage <- get_map(location = c(lon=174.7633, lat=-36.8485), zoom = 9)
-
-proj4string <- "+proj=nzmg +lat_0=-41.0 +lon_0=173.0 +x_0=2510000.0 +y_0=6023150.0 +ellps=intl +units=m"
-
+# Auckland
+# mapImage <- get_map(location = c(lon=174.7633, lat=-36.8485), zoom = 9)
+# proj4string <- "+proj=nzmg +lat_0=-41.0 +lon_0=173.0 +x_0=2510000.0 +y_0=6023150.0 +ellps=intl +units=m"
 # area <- readShapePoly("/home/ubuntu/rstats/shapefiles/AU_RC_Auckland_RC")
-area <- readOGR("/home/ubuntu/rstats/shapefiles/AU_RC_Auckland_RC")
+
+
+
+# Dunedin
+mapImage <- get_map(location = c(lon=170.5028, lat=-45.8788), zoom = 9)
+proj4string <- "+proj=nzmg +lat_0=-41.0 +lon_0=173.0 +x_0=2510000.0 +y_0=6023150.0 +ellps=intl +units=m"
+area <- readShapePoly("/home/ubuntu/rstats/shapefiles/AU_TA_Dunedin_CC")
+
+
 
 area.points <- fortify(area)
 
