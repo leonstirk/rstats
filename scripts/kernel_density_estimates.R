43327a0a5b087403de093a9788373467b578967e
Leon Stirk-Wang <leon.stirkwang@gmail.com>
2018-06-22 01:30:01 +1200

    auto-backup
    Former-commit-id: 9e928eeb09575f29f6d2dc389d6093864ec78622
    Former-commit-id: ba884db426b55034527a7379be12686cfc8d063a

--- scripts/kernel_density_estimates.R
+++ scripts/kernel_density_estimates.R
@@ -7,11 +7,12 @@ a <- das_caversham
 n <- length(levels(as.factor(a$sale_year)))
 colours <- rainbow(n, alpha = 1)
 linetype <- rep(1, n)
+
 xlabs <- das_vars[c(1,2,3,4,5,6,8,9,10,11)]
 # xlabs <- das_vars[c(1)]
 dates <- c('2002', '2008')
 
-layout(matrix(c(1,2,3,4,5,6,7,8,9,10,11,12),3,4)) # optional 4 graphs/page
+layout(matrix(seq(1,12,1),3,4)) # optional 4 graphs/page
 
 for(xlab in xlabs) {
 
--- scripts/prs_m.R
+++ scripts/prs_m.R
@@ -1,16 +1,28 @@
 source('scripts/das_data_preprocessing.R')
 
-## generate quarterly price index across all data (no spatial submarkets)
+library('sm')
 
 a <- das_caversham
 
 mean_summary <- a %>% group_by(sale_year) %>% dplyr::summarise(n_sale_year = n(), mean_ln_sale_price = mean(ln_sale_price), std_error = sd(ln_sale_price)/sqrt(n_sale_year))
 
-t <- as.numeric(levels(as.factor(a$sale_year)))
-ti <- t[2]
+t_vec <- as.numeric(levels(as.factor(a$sale_year)))
 
-a_sub <- subset(a, a$sale_year == ti | a$sale_year == ti+1)
-a_sub$treatment <- ifelse(a_sub$sale_year == ti, 0 ,1)
+# layout(matrix(seq(1,15,1),3,5)) # optional 12 graphs/page;
+
+## Loop over each base year
+# for(t0 in t_vec) {
+t0 <- '2000'
+
+  ## Loop over each year pair
+  # for (t1 in t_vec) {
+  t1 <- '2001'
+
+    ## Only match where t0 != t1
+    # if(t0 != t1) {
+
+a_sub <- subset(a, a$sale_year == t0 | a$sale_year == t1)
+a_sub$treatment <- ifelse(a_sub$sale_year == t0, 0 ,1)
 
 # non-covariate-adjusted (non-matched) difference in mean test for log net sale price between treatment and control groups
 t_test_output <- with(a_sub, t.test(ln_sale_price ~ sale_year))
@@ -29,21 +41,37 @@ a_sub_pretreatment_covariate_sig_dummy <- ifelse(a_sub_pretreatment_covariate_p_
 a_sub_pretreatment_cov_test_tables <- data.frame(a_sub_cov, a_sub_pretreatment_covariate_t_stats, a_sub_pretreatment_covariate_p_values, a_sub_pretreatment_covariate_sig_dummy)
 
 # propensity score estimation
-covariates<- a_sub %>% dplyr::select(QPID, ln_sale_price, treatment, one_of(a_sub_cov))
-m_ps <- glm(treatment ~ ., family = binomial(), data = covariates)
+
+# model strings
+model_lhs_vars <- paste(a_sub_cov, collapse = " + ")
+matchmodelstring <- paste("treatment ~ ",model_lhs_vars)
+
+m_ps <- glm(matchmodelstring, family = binomial(), data = a_sub)
 prs_df <- data.frame(pr_score = predict(m_ps, type = "response"), treatment = m_ps$model$treatment)
 
 # Visual check of propensity score based distributional matching
 # Kernel density estimates of propensity scores by treatment (t0, t1)
 sm.density.compare(prs_df$pr_score, prs_df$treatment)
 
+#    }
+#  }
+# }
+
+
+
 # Execute matching algorithm
-a_sub_nomiss <- a_sub %>% select(QPID, sale_id, sale_quarter, ln_sale_price, treatment, one_of(a_sub_cov)) %>% na.omit()
+a_sub_nomiss <- a_sub %>% dplyr::select(QPID, sale_id, sale_year, ln_sale_price, treatment, one_of(a_sub_cov)) %>% na.omit()
 
-modelstring <- 
+mod_match <- matchit(treatment ~  bedrooms + bathrooms + carparks + offstreet_parking + deck + ex_state_house + contour + period_built + ln_building_floor_area + ln_land_area, method = "nearest", data = a_sub_nomiss)
 
+dta_m <- match.data(mod_match)
 
-mod_match <- matchit(modelstring, method = "nearest", data = das_sub_nomiss)
+source('functions/fn_bal.R')
 
-# dta_m <- match.data(mod_match)
+fn_bal(dta_m, "bedrooms")
 
+TE <- data.frame(dta_m[rownames(mod_match$match.matrix),"ln_sale_price"],dta_m[mod_match$match.matrix,"ln_sale_price"])
+
+names(TE) <- c('ln_sale_t1','ln_sale_t0')
+
+TE$diff <- TE$ln_sale_t1 - TE$ln_sale_t0
