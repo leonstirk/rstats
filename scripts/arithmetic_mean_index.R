5d0afbc9d3a33d20c9a8370f9d753402f226db36
Leon Stirk-Wang <leon.stirkwang@gmail.com>
2018-07-19 01:30:01 +1200

    auto-backup
    Former-commit-id: e7f848e89c22d4ee15aa5d9b253904672ee17e6e
    Former-commit-id: ab5e50e7325d0985ce17b322a0158cd2ca459ee5

--- scripts/arithmetic_mean_index.R
+++ scripts/arithmetic_mean_index.R
@@ -23,3 +23,20 @@ names(meanIndexByAU) <- c('year', 'area_unit_name', 'index')
 # nSalesByPeriod <- function(data, period) {
 #  return(aggregate(data[,c('ln_sale_price')],list(data$sale_year),length)[,2]))
 # }
+
+exampleTable <- meanIndexByAU[which(meanIndexByAU$area_unit_name %in% c('Opoho', 'Forrester Park', 'North East Valley', 'Pine Hill')),]
+exampleTable <- exampleTable[which(exampleTable$year %in% c(2000:2005)),]
+exampleTable$index <- exampleTable$index*100
+
+
+explot <- ggplot(data=exampleTable, aes(x=year, y=index, color=area_unit_name)) + geom_line(aes(group = area_unit_name))
+
+
+exampleTable$index <- format(exampleTable$index, digits=1, nsmall=1)
+
+exampleTable <- t(dcast(exampleTable, year ~ area_unit_name, value.var="index"))
+
+names <- exampleTable[1,]
+exampleTable <- data.frame(tail(exampleTable, -1))
+names(exampleTable) <- names
+rm(names)
