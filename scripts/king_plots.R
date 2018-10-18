f525e31b7cb5060438f47ae11cc279c2aca9fc6f
Leon Stirk-Wang <leon.stirkwang@gmail.com>
2018-10-18 18:00:54 +1300

    auto-backup

--- scripts/king_plots.R
+++ scripts/king_plots.R
@@ -1,9 +1,16 @@
 source('scripts/mah_m.R')
 
-layout(matrix(seq(1,12,1),3,4)) # optional 12 graphs/page
+library(plyr)
+library(gridExtra)
+
+layout(matrix(seq(1,1,1),2,1))
 
 a_sub_nomiss$treatment_f<-mapvalues(a_sub_nomiss$treatment, from = c(0, 1), to = c("C", "T"))
-ggplot(data=a_sub_nomiss, aes(x=ln_land_area, y=ln_building_floor_area, color=treatment_f)) + geom_text(aes(label=treatment_f)) + scale_colour_manual(values=c("Red","Blue"))
+
+a_plot <- ggplot(data=a_sub_nomiss, aes(x=ln_land_area, y=ln_building_floor_area, color=treatment_f)) + geom_text(aes(label=treatment_f)) + scale_colour_manual(values=c("Red","Blue")) + theme(legend.position="none")
 
 m_data$treatment_f<-mapvalues(m_data$treatment, from = c(0, 1), to = c("C", "T"))
-ggplot(data=m_data, aes(x=ln_land_area, y=ln_building_floor_area, color=treatment_f)) + geom_text(aes(label=treatment_f)) + scale_colour_manual(values=c("Red","Blue"))
+
+b_plot <- ggplot(data=m_data, aes(x=ln_land_area, y=ln_building_floor_area, color=treatment_f)) + geom_text(aes(label=treatment_f)) + scale_colour_manual(values=c("Red","Blue")) + theme(legend.position="none")
+
+grid.arrange(a_plot, b_plot, ncol=2)
