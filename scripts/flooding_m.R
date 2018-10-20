bc346c4140fe4ef6682318107ffd7467ffdf4975
Leon Stirk-Wang <leon.stirkwang@gmail.com>
2018-10-20 16:15:45 +1300

    auto-backup

--- scripts/flooding_m.R
+++ scripts/flooding_m.R
@@ -1,9 +1,10 @@
-source('scripts/flooding_data_processing.R')
+# source('scripts/flooding_data_processing.R')
 
 before_flood <- subset(flood_sub, after_flood == 0)
 after_flood <- subset(flood_sub, after_flood ==1)
 
-a_sub <- before_flood
+a_sub <- after_flood
+a_sub$treatment <- ifelse(a_sub$flooded == 1,0,1)
 
 ###################################################
 ## Define covariate space for pre-matched samples #
@@ -13,14 +14,14 @@ a_sub_cov<-tail(das_vars,-1)
 ###############################################################
 ## Omit any observations with missing values in the variables #
 ###############################################################
-a_sub_nomiss <- a_sub %>% dplyr::select(qpid, sale_id, sale_year, ln_sale_price, flooded, one_of(a_sub_cov)) %>% na.omit()
+a_sub_nomiss <- a_sub %>% dplyr::select(qpid, sale_id, sale_year, ln_sale_price, treatment, one_of(a_sub_cov)) %>% na.omit()
 
 ########################
 # Create model strings #
 ########################
 
 model_lhs_vars <- paste(a_sub_cov, collapse = " + ")
-model_formula <- as.formula(paste("flooded ~ ",model_lhs_vars))
+model_formula <- as.formula(paste("treatment ~ ",model_lhs_vars))
 
 ################################
 ## Nearest match (mahalanobis) #
@@ -35,10 +36,10 @@ m_data <- match.data(m_out)
 ####################
 ## Balance summary #
 ####################
-b_sum <- summary(m_out)
-b_sum_std <- summary(m_out, standardize = TRUE)
-b_plot <- plot(m_out)
-b_plot_sum <- plot(b_sum_std)
+# b_sum <- summary(m_out)
+# b_sum_std <- summary(m_out, standardize = TRUE)
+# b_plot <- plot(m_out)
+# b_plot_sum <- plot(b_sum_std)
 
 ###################
 ## Zelig analysis #
@@ -48,3 +49,13 @@ b_plot_sum <- plot(b_sum_std)
 # t_out <- setx(z_out, treatment = 1)
 # s_out <- sim(z_out, c_out, t_out)
 
+
+
+# before_flood_m$after <- 0
+# after_flood_m$after <- 1
+
+# diff_in_diff_data <- rbind(before_flood_m, after_flood_m)
+# diff_in_diff_data$flooded <- 1-diff_in_diff_data$treatment
+
+# fit <- lm(ln_sale_price ~ after + flooded + after*flooded + bedrooms + bathrooms + ln_land_area + ln_building_floor_area, data=diff_in_diff_data)
+# summary(fit)
