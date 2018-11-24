eb7a8589f6b04575f06dfc27bb838d57b20c9fce
Leon Stirk-Wang <leon.stirkwang@gmail.com>
2018-11-24 17:30:01 +1300

    auto-backup

--- scripts/kernel_density_estimates.R
+++ scripts/kernel_density_estimates.R
@@ -15,3 +15,18 @@ sm.density.compare(flood_sub$homeowner_rate, flood_sub$treatment, xlim = c(0,1),
 sm.density.compare(dnd_sub$homeowner_rate, dnd_sub$treatment, xlim = c(0,1), ylim = c(0,3))
 
 layout(matrix(seq(1,2,1),2,1))
+
+sm.density.compare(flood_sub$building_floor_area, flood_sub$treatment, xlim = c(0,400), ylim = c(0,0.02))
+sm.density.compare(dnd_sub$building_floor_area, dnd_sub$treatment, xlim = c(0,400), ylim = c(0,0.02))
+
+
+sm.density.compare(flood_sub$building_floor_area, flood_sub$treatment, xlim = c(0,400), ylim = c(0,0.02))
+sm.density.compare(dnd_sub$building_floor_area, dnd_sub$treatment, xlim = c(0,400), ylim = c(0,0.02))
+
+
+sm.density.compare(flood_sub$building_floor_area, flood_sub$treatment, xlim = c(0,400), ylim = c(0,0.02))
+sm.density.compare(dnd_sub$building_floor_area, dnd_sub$treatment, xlim = c(0,400), ylim = c(0,0.02))
+
+
+sm.density.compare(flood_sub$building_floor_area, flood_sub$treatment, xlim = c(0,400), ylim = c(0,0.02))
+sm.density.compare(dnd_sub$building_floor_area, dnd_sub$treatment, xlim = c(0,400), ylim = c(0,0.02))
