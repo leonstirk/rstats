1201f2d097c7437ce55c50a94781a4db1b6cfc12
Leon Stirk-Wang <leon.stirkwang@gmail.com>
2018-11-26 17:30:01 +1300

    auto-backup

--- scripts/kernel_density_estimates.R
+++ scripts/kernel_density_estimates.R
@@ -2,31 +2,33 @@ require(sm)
 
 layout(matrix(seq(1,8,1),4,2))
 
-sm.density.compare(flood_sub$building_floor_area, flood_sub$treatment, xlim = c(0,400), ylim = c(0,0.02))
-sm.density.compare(dnd_sub$building_floor_area, dnd_sub$treatment, xlim = c(0,400), ylim = c(0,0.02))
+unmatched <- flood_sub
+matched <- dnd_sub
 
-sm.density.compare(flood_sub$land_area, flood_sub$treatment, xlim = c(0,2000), ylim = c(0,0.005))
-sm.density.compare(dnd_sub$land_area, dnd_sub$treatment, xlim = c(0,2000), ylim = c(0,0.005))
+unmatched_before <- unmatched[which(unmatched$after_flood == 0),]
+unmatched_after <- unmatched[which(unmatched$after_flood == 1),]
 
-sm.density.compare(flood_sub$median_income, flood_sub$treatment, xlim = c(0,15), ylim = c(0,0.3))
-sm.density.compare(dnd_sub$median_income, dnd_sub$treatment, xlim = c(0,15), ylim = c(0,0.3))
+matched_before <- matched[which(matched$after_flood == 0),]
+matched_after <- matched[which(matched$after_flood == 1),]
 
-sm.density.compare(flood_sub$homeowner_rate, flood_sub$treatment, xlim = c(0,1), ylim = c(0,3))
-sm.density.compare(dnd_sub$homeowner_rate, dnd_sub$treatment, xlim = c(0,1), ylim = c(0,3))
+rm(unmatched)
+rm(matched)
 
-layout(matrix(seq(1,2,1),2,1))
+densityCompare <- function(unmatched, matched) {
 
-sm.density.compare(flood_sub$building_floor_area, flood_sub$treatment, xlim = c(0,400), ylim = c(0,0.02))
-sm.density.compare(dnd_sub$building_floor_area, dnd_sub$treatment, xlim = c(0,400), ylim = c(0,0.02))
+    sm.density.compare(unmatched$building_floor_area, unmatched$treatment, xlim = c(0,400), ylim = c(0,0.02))
+    sm.density.compare(matched$building_floor_area, matched$treatment, xlim = c(0,400), ylim = c(0,0.02))
 
+    sm.density.compare(unmatched$land_area, unmatched$treatment, xlim = c(0,2000), ylim = c(0,0.005))
+    sm.density.compare(matched$land_area, matched$treatment, xlim = c(0,2000), ylim = c(0,0.005))
 
-sm.density.compare(flood_sub$building_floor_area, flood_sub$treatment, xlim = c(0,400), ylim = c(0,0.02))
-sm.density.compare(dnd_sub$building_floor_area, dnd_sub$treatment, xlim = c(0,400), ylim = c(0,0.02))
+    sm.density.compare(unmatched$median_income, unmatched$treatment, xlim = c(0,15), ylim = c(0,0.3))
+    sm.density.compare(matched$median_income, matched$treatment, xlim = c(0,15), ylim = c(0,0.3))
 
+    sm.density.compare(unmatched$homeowner_rate, unmatched$treatment, xlim = c(0,1), ylim = c(0,3))
+    sm.density.compare(matched$homeowner_rate, matched$treatment, xlim = c(0,1), ylim = c(0,3))
 
-sm.density.compare(flood_sub$building_floor_area, flood_sub$treatment, xlim = c(0,400), ylim = c(0,0.02))
-sm.density.compare(dnd_sub$building_floor_area, dnd_sub$treatment, xlim = c(0,400), ylim = c(0,0.02))
+}
 
-
-sm.density.compare(flood_sub$building_floor_area, flood_sub$treatment, xlim = c(0,400), ylim = c(0,0.02))
-sm.density.compare(dnd_sub$building_floor_area, dnd_sub$treatment, xlim = c(0,400), ylim = c(0,0.02))
+## densityCompare(unmatched_before, matched_before)
+## densityCompare(unmatched_after, matched_after)
